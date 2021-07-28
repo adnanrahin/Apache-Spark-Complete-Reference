9c18b4d4e5387e7332dc53d8cd5692597685520e
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-28 16:42:35 -0400

    update

--- src/main/scala/org/apache/spark/samplescala/TrimGithubEmployees.scala
+++ src/main/scala/org/apache/spark/samplescala/TrimGithubEmployees.scala
@@ -1,9 +1,27 @@
 package org.apache.spark.samplescala
 
+import scala.io.Source.fromFile
+
 object TrimGithubEmployees {
 
   def main(args: Array[String]): Unit = {
-    
+
+    val ghPath = "src/main/scala/org/apache/spark/samplescala/ghEmployees.txt"
+
+    val employees = Set() ++ (
+        for {
+          line <- fromFile(ghPath).getLines
+        } yield line.trim
+      )
+
+    println(employees.getClass)
+
+    val findEmployeesNameStartWithVowel =
+      employees.filter(name => name.startsWith("a"))
+
+    println(findEmployeesNameStartWithVowel.toString())
+    println(findEmployeesNameStartWithVowel.getClass)
+
   }
 
 }
