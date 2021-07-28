17b2861327ba4cdd1b12f65e23f26f27184eb13c
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-28 17:54:22 -0400

    Text Data changes

--- src/main/scala/org/apache/spark/samplescala/TrimGithubEmployees.scala
+++ src/main/scala/org/apache/spark/samplescala/TrimGithubEmployees.scala
@@ -1,17 +1,25 @@
 package org.apache.spark.samplescala
 
+import org.apache.spark.sql.SparkSession
+
 import scala.io.Source.fromFile
 
 object TrimGithubEmployees {
 
   def main(args: Array[String]): Unit = {
 
-    val ghPath = "src/main/scala/org/apache/spark/samplescala/ghEmployees.txt"
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .appName("GitHubArchive")
+      .getOrCreate()
+
+    val ghPath = "textdata/ghEmployees.txt"
 
     val employees = Set() ++ (
-        for {
-          line <- fromFile(ghPath).getLines
-        } yield line.trim
+      for {
+        line <- fromFile(ghPath).getLines
+      } yield line.trim
       )
 
     println(employees.getClass)
@@ -23,6 +31,14 @@ object TrimGithubEmployees {
     println(filetEmployees.getClass)
 
     val isEmp: (String => Boolean) = (arg: String) => employees.contains(arg)
+
+    val isEmployee = spark.udf.register("isEmpUdf", isEmp)
+
+    println(isEmployee)
+
+    val isIn = isEmp("add")
+
+    println(isIn)
 
   }
 
