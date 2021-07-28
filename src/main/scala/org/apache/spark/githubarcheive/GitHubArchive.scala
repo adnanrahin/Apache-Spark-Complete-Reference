6e72a208e10547312cffa5e2b02c06a7bc7897f6
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-28 19:10:38 -0400

    update

--- src/main/scala/org/apache/spark/githubarcheive/GitHubArchive.scala
+++ src/main/scala/org/apache/spark/githubarcheive/GitHubArchive.scala
@@ -4,6 +4,8 @@ import org.apache.spark.listhdfs.ListHdfsFiles
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.col
 
+import scala.io.Source.fromFile
+
 object GitHubArchive {
 
   def main(args: Array[String]): Unit = {
@@ -34,7 +36,7 @@ object GitHubArchive {
 
     println(actorLoginPushEvent.show(5))
 
-    /** actorLoginPushEven data set order by desc**/
+    /** actorLoginPushEven data set order by desc* */
 
     val actorLoginOrder = pushEvent
       .groupBy("actor.login")
@@ -42,6 +44,20 @@ object GitHubArchive {
       .orderBy(col("count").desc)
 
     println(actorLoginOrder.show(5))
+
+    val employeesSet = Set() ++ (
+      for {
+        line <- fromFile("textdata/ghEmployees.txt").getLines
+      } yield line.trim)
+
+    val broadCastEmployeeSet = sc.broadcast(employeesSet)
+
+
+    import spark.implicits._
+    val isEmp = user => broadCastEmployeeSet.value.contains(user)
+    val isEmployee = spark.udf.register("SetContainsUdf", isEmp)
+    val filtered = actorLoginOrder.filter(isEmployee($"login"))
+    println(filtered.show(5))
 
   }
 
