587a49cfb3be53278c3e1d1a4575c86305ce00d2
Adnan Rahin <adnan.rahin13@gmail.com>
2021-07-29 01:51:17 -0400

    Finding customer id and purchase

--- src/main/scala/org/apache/spark/customertransaction/CustomerTransaction.scala
+++ src/main/scala/org/apache/spark/customertransaction/CustomerTransaction.scala
@@ -1,10 +1,42 @@
 package org.apache.spark.customertransaction
 
+import org.apache.spark.sql.SparkSession
+
 object CustomerTransaction {
 
   def main(args: Array[String]): Unit = {
 
-    
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .appName("CustomerTransaction")
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+
+    val transactionFile = sc.textFile("textdata/ch04_data_transactions.txt")
+
+    val transactionData = transactionFile.map(_.split("#"))
+
+    transactionData.foreach(row => println(row.mkString("Array(", ", ", ")")))
+
+    val transactionByCustomer = transactionData.map(transaction => (transaction(2).toInt, transaction))
+
+    transactionByCustomer.foreach(row => println(row._2.mkString("Array(", ", ", ")")))
+
+    /** Creating new map to count number of transaction is completed by each customer* */
+
+    val countTransactionByCustomer =
+      transactionByCustomer.countByKey()
+
+    println(countTransactionByCustomer)
+
+    /** Find which customer made the most purchase* */
+
+    val (cid, purchase) = transactionByCustomer
+      .countByKey().toSeq.maxBy(_._2)
+
+    println("Customer ID: " + cid + " Purchase: " + purchase)
 
   }
 
