cc8230fb385141d1a846d2cb34aff986bd9cdc7a
Adnan Rahin <adnan.rahin13@gmail.com>
2021-08-07 23:14:21 -0400

    Spark Session Object

--- src/main/scala/org/apache/spark/learning_spark/SparkDriverProgram.scala
+++
@@ -1,5 +0,0 @@
-package org.apache.spark.learning_spark
-
-object SparkDriverProgram {
-
-}
--- src/main/scala/org/apache/spark/learning_spark/chapter_two/SparkDriverProgram.scala
+++ src/main/scala/org/apache/spark/learning_spark/chapter_two/SparkDriverProgram.scala
@@ -1,5 +1,16 @@
 package org.apache.spark.learning_spark.chapter_two
 
+import org.apache.spark.sql.SparkSession
+
 object SparkDriverProgram {
 
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .appName("GitHubArchive")
+      .getOrCreate()
+
+  }
 }
