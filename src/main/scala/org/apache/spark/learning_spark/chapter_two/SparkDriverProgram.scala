fb0b2994472c6f441687531045394133c023e560
Adnan Rahin <adnan.rahin13@gmail.com>
2021-08-08 20:58:48 -0400

    update

--- src/main/scala/org/apache/spark/learning_spark/chapter_two/SparkDriverProgram.scala
+++ src/main/scala/org/apache/spark/learning_spark/chapter_two/SparkDriverProgram.scala
@@ -1,6 +1,7 @@
 package org.apache.spark.learning_spark.chapter_two
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.column
 
 object SparkDriverProgram {
 
@@ -15,11 +16,15 @@ object SparkDriverProgram {
     val sc = spark.sparkContext
 
     if (args.length < 1) {
-      print("Usage: MnMcount <mnm_file_dataset>")
+      print("Usage: MnMCount <mnm_file_dataset>")
       sys.exit(1)
     }
 
     val mnmFile = args(0)
+
+    /**
+     * Loading data into Spark DataFrame
+     * */
 
     val mnmDf = spark.read.format("csv")
       .option("header", "true")
@@ -27,6 +32,15 @@ object SparkDriverProgram {
       .load(mnmFile)
 
     println(mnmDf.show(5))
+
+    /**
+     *  Aggregate counts of all colors and groupBy() state and Color
+     *  OrderBy() in descending order
+     * */
+
+    val countMndMDF = mnmDf
+      .select(column("State"), column("Color"), column("Count"))
+
 
   }
 }
