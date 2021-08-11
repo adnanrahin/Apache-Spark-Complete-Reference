3325e4c3b246e730b47d4fa6a895a912bf81b982
Adnan Rahin <adnan.rahin13@gmail.com>
2021-08-10 21:55:47 -0400

    Rational Class Created

---
+++ programming-in-scala/src/main/scala/org/programming/scala/functional_objects/FunctionalObjects.scala
@@ -0,0 +1,21 @@
+package org.programming.scala.functional_objects
+
+object FunctionalObjects {
+
+  class Rational(n: Int, d: Int){
+
+    /*Override twString method*/
+
+    override def toString: String = n + "/" + d
+
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val oneHalf = new Rational(1, 2)
+
+    println(oneHalf)
+
+  }
+
+}
