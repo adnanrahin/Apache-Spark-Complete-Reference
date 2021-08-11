641770401b5e99c76b6b980cc80575751b5d6db8
adnanrahin <adnan.rahin13@gmail.com>
2021-08-10 22:49:06 -0400

    Update

--- programming-in-scala/src/main/scala/org/programming/scala/functional_objects/FunctionalObjects.scala
+++ programming-in-scala/src/main/scala/org/programming/scala/functional_objects/FunctionalObjects.scala
@@ -2,14 +2,22 @@ package org.programming.scala.functional_objects
 
 object FunctionalObjects {
 
-  class Rational(n: Int, d: Int){
+  class Rational(n: Int, d: Int) {
 
-    /*Override twString method*/
+    require(d != 0)
+
+    val numer: Int = n
+    val denom: Int = d
 
     override def toString: String = n + "/" + d
 
+    def add(that: Rational): Rational =
+      new Rational(
+        numer * that.denom + that.numer * denom,
+        denom * that.denom
+      )
   }
- 
+
   def main(args: Array[String]): Unit = {
 
     val oneHalf = new Rational(1, 2)
