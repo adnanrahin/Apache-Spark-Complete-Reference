163ebc6536437b892ff55d92b598085d7d44c73d
adnanrahin <adnan.rahin13@gmail.com>
2021-08-12 16:06:51 -0400

    FilesMatching

--- programming-in-scala/src/main/scala/org/programming/scala/control_abstraction/ControlAbstraction.scala
+++ programming-in-scala/src/main/scala/org/programming/scala/control_abstraction/ControlAbstraction.scala
@@ -19,8 +19,22 @@ object ControlAbstraction {
   def findFilesByEndNames(query: String, files: Array[File]): Array[File] = {
     for {
       file <- files
-      if (file.getName.contains(query))
+      if (file.getName.endsWith(query))
     } yield file
   }
+
+  def findFilesByMatch(query: String, files: Array[File]): Array[File] = {
+    for {
+      file <- files
+      if (file.getName.matches(query))
+    } yield file
+  }
+
+  def filesMatching(query: String, matcher: (String, String) => Boolean): Array[File] = {
+    for (file <- filesHere; if matcher(file.getName, query))
+      yield file
+  }
+
+  def filesEnding(query: String): Array[File] = filesMatching(query, _.endsWith(_))
 
 }
