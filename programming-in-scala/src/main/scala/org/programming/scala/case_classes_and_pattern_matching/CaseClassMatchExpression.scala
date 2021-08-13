82c4cefb6b986e14bb940ee4c337caa2b29f1e9a
adnanrahin <adnan.rahin13@gmail.com>
2021-08-12 22:48:29 -0400

    Case Class Pattern Matching

---
+++ programming-in-scala/src/main/scala/org/programming/scala/case_classes_and_pattern_matching/CaseClassMatchExpression.scala
@@ -0,0 +1,32 @@
+package org.programming.scala.case_classes_and_pattern_matching
+
+object CaseClassMatchExpression {
+
+  abstract class Animal
+
+  case class Mammal(name: String, fromSea: Boolean) extends Animal
+
+  case class Bird(name: String) extends Animal
+
+  case class Fish(name: String) extends Animal
+
+  def main(args: Array[String]): Unit = {
+
+    val mammal =  Mammal("Shark", fromSea = true)
+
+    val getDescription = caseClassPatternMatchExpression(mammal)
+
+    println(getDescription.substring(10))
+
+  }
+
+  def caseClassPatternMatchExpression(animal: Animal): String = {
+    animal match {
+      case Mammal(name, fromSea) => s"Mammal name is $name, is it from sea $fromSea"
+      case Bird(name) => s"Bird Name is $name"
+      case Fish(name) => s"Fish Name is $name"
+      case _ => "This animal does not match anything"
+    }
+  }
+
+}
