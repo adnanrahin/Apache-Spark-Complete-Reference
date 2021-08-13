2a58fb208b43ffffed6630320144278e81a9d814
adnanrahin <adnan.rahin13@gmail.com>
2021-08-13 00:59:28 -0400

    update

--- programming-in-scala/src/main/scala/org/programming/scala/case_classes_and_pattern_matching/SortNDArrays.scala
+++ programming-in-scala/src/main/scala/org/programming/scala/case_classes_and_pattern_matching/SortNDArrays.scala
@@ -4,7 +4,19 @@ object SortNDArrays {
 
   def main(args: Array[String]): Unit = {
 
+    val sortedList = sortWithPatternMatching(Array(5, 6, 7, 3))
 
+    println(sortedList)
+  }
+
+  def sortWithPatternMatching(anyArray: Array[Integer]): Any = {
+
+    anyArray match {
+      case Array(_*) => {
+        val arrToList = anyArray.toList.sorted
+        arrToList
+      }
+    }
 
   }
 
