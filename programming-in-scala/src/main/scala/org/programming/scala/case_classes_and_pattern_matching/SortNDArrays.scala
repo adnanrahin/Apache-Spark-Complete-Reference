e003cb5186c34e745f8782786e2752b3e2c393d9
adnanrahin <adnan.rahin13@gmail.com>
2021-08-13 01:38:55 -0400

    Update

--- programming-in-scala/src/main/scala/org/programming/scala/case_classes_and_pattern_matching/SortNDArrays.scala
+++ programming-in-scala/src/main/scala/org/programming/scala/case_classes_and_pattern_matching/SortNDArrays.scala
@@ -5,18 +5,33 @@ object SortNDArrays {
   def main(args: Array[String]): Unit = {
 
     val sortedList = sortWithPatternMatching(Array(5, 6, 7, 3))
-
     println(sortedList)
     println(sortedList.getClass)
+
+    val getSortedStringListByLength = sortWithPatternMatching(Array("Bad", "Book", "Megatron", "Optimus"))
+    println(getSortedStringListByLength)
+    println(getSortedStringListByLength.getClass)
+
+    val getSortedStringToCharArray = sortWithPatternMatching("kbacdef")
+    println(getSortedStringToCharArray)
   }
 
-  def sortWithPatternMatching(anyArray: Array[Integer]): Any = {
+  def sortWithPatternMatching(anyArray: Any): Any = {
 
     anyArray match {
-      case Array(_*) => {
-        val arrToList = anyArray.toList.sorted
-        arrToList
+      case x: Array[Int] => {
+        val sortedArray = x.sorted.toList
+        sortedArray
       }
+      case x: Array[String] => {
+        val sortedString = x.toList.sortWith(_.length > _.length)
+        sortedString
+      }
+      case x: String => {
+        val charSortedArray = x.toCharArray.sorted.mkString(",")
+        charSortedArray
+      }
+      case _ => println("whatever")
     }
 
   }
