b64844625052717b5b3c1d1fb8b68946bc661bde
adnanrahin <adnan.rahin13@gmail.com>
2021-08-14 01:02:40 -0400

    Get List from the typedPatternMatch

--- programming-in-scala/src/main/scala/org/programming/scala/case_classes_and_pattern_matching/TypedPatternMatching.scala
+++ programming-in-scala/src/main/scala/org/programming/scala/case_classes_and_pattern_matching/TypedPatternMatching.scala
@@ -5,10 +5,14 @@ object TypedPatternMatching {
   def main(args: Array[String]): Unit = {
 
     val getPatternValue = typedPatternMatching("My Name is Megatron")
+    println(getPatternValue)
 
+    val getPatternList = typedPatternMatching(List(1, 3, 4, 5, 5))
+    println(getPatternList)
+    
   }
 
-  def typedPatternMatching(value: Any): Any = {
+  def typedPatternMatching(value: Any): String = {
     value match {
       case x: String => s"$value is String"
       case x: Int => s"$value is Integer"
