e693fd5e1f8f862d29f4c58c015940ae357289f6
adnanrahin <adnan.rahin13@gmail.com>
2021-08-15 22:37:23 -0400

    update

---
+++ programming-in-scala/src/main/scala/org/programming/scala/working_with_lists/ListLiterals.scala
@@ -0,0 +1,27 @@
+package org.programming.scala.working_with_lists
+
+object ListLiterals {
+
+  def main(args: Array[String]): Unit = {
+
+      println(appendListValues(List("Transformer", "Cybertruck", "Audi")))
+
+  }
+
+  def appendListValues(list: List[Any]): List[Any] = {
+
+    list match {
+      case x: List[String] => {
+        val newList = x.map(str => str + "a")
+        newList
+      }
+      case x: List[Integer] => {
+        val newList = x.map(value => value + 1)
+        newList
+      }
+      case _ => Nil
+    }
+
+  }
+
+}
