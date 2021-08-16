7087b215ceb1e626b642319f96a1b60b54e21086
adnanrahin <adnan.rahin13@gmail.com>
2021-08-16 16:09:10 -0400

    update

--- programming-in-scala/src/main/scala/org/programming/scala/working_with_lists/HigherOrderMethod.scala
+++ programming-in-scala/src/main/scala/org/programming/scala/working_with_lists/HigherOrderMethod.scala
@@ -4,8 +4,23 @@ object HigherOrderMethod {
 
   def main(args: Array[String]): Unit = {
 
-    
+    println(addOne(List(1, 2, 3, 4, 5, 6)))
+    println(getLengthOfAllStrings(List("megatron", "time", "travel")))
+    println(reverseStringsInList(List("megatron", "time", "travel")))
+  }
 
+  def addOne(list: List[Int]): List[Int] = {
+    val newList = list.map(_ + 1)
+    newList
+  }
+
+  def getLengthOfAllStrings(strs: List[String]): List[Int] = {
+    val newList = strs.map(_.length)
+    newList
+  }
+
+  def reverseStringsInList(list: List[String]): List[String] = {
+    list.map(_.reverse)
   }
 
 }
