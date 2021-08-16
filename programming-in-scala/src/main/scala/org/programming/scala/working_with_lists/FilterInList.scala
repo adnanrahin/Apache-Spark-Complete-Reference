17c80125d08c7aec300aad355234bbaeaa401e51
adnanrahin <adnan.rahin13@gmail.com>
2021-08-16 18:26:12 -0400

    Odd Even List

--- programming-in-scala/src/main/scala/org/programming/scala/working_with_lists/FilterInList.scala
+++ programming-in-scala/src/main/scala/org/programming/scala/working_with_lists/FilterInList.scala
@@ -6,6 +6,11 @@ object FilterInList {
 
     println(filterEvenNumbers(List(3, 4, 5, 6, 7, 8, 9, 90)))
 
+    val even, odd = listPartition(List(1, 2, 3, 4, 5, 6, 7, 8))
+
+    println("Odd :" + odd)
+    println("Even " + even)
+
   }
 
   def filterEvenNumbers(list: List[Int]): List[Int] = {
@@ -13,6 +18,10 @@ object FilterInList {
 
     val newList = list.filter(isEven)
     newList
+  }
+
+  def listPartition(list: List[Int]): (List[Int], List[Int]) = {
+    list.partition(_ % 2 == 0)
   }
 
 }
