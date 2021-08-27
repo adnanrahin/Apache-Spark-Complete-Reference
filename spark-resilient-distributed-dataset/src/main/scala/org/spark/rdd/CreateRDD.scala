10e0bbfaa9e754ab78ef10ce0fdff009bd04ce41
adnanrahin <adnan.rahin13@gmail.com>
2021-08-26 23:21:33 -0400

    update

---
+++ spark-resilient-distributed-dataset/src/main/scala/org/spark/rdd/CreateRDD.scala
@@ -0,0 +1,22 @@
+package org.spark.rdd
+
+import org.apache.spark.sql.SparkSession
+
+object CreateRDD {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("CreateRDD")
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+
+    val rdd = sc.textFile("textdata/ch04_data_products.txt")
+
+    rdd.foreach(str => println(str))
+
+  }
+
+}
