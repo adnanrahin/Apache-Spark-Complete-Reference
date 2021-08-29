e534a4c1f6383b407d7a2c0f0987dec111e2eb29
adnanrahin <adnan.rahin13@gmail.com>
2021-08-28 23:54:47 -0400

    update

--- spark-resilient-distributed-dataset/src/main/scala/org/spark/rdd/ParallelizeRDD.scala
+++ spark-resilient-distributed-dataset/src/main/scala/org/spark/rdd/ParallelizeRDD.scala
@@ -7,7 +7,7 @@ object ParallelizeRDD {
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder()
-      .master("local[*]")
+      .master("local[1]")
       .appName("ParallelizeRDD")
       .getOrCreate()
 
@@ -15,7 +15,11 @@ object ParallelizeRDD {
 
     val rdd = sc.parallelize(List(1, 2, 3, 4, 5, 6, 7))
 
-    
+    val rddCollect:Array[Int] = rdd.collect()
+    println("Number of Partitions: "+rdd.getNumPartitions)
+    println("Action: First element: "+rdd.first())
+    println("Action: RDD converted to Array[Int] : ")
+    rddCollect.foreach(println)
 
   }
 
