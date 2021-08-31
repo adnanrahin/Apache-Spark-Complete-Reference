ef71b8948409721b8d3e74108dc5eb2d41b2b28a
adnanrahin <adnan.rahin13@gmail.com>
2021-08-31 01:10:57 -0400

    Filght Rdd Created from CSV

--- spark-resilient-distributed-dataset/src/main/scala/org/spark/rdd/flights_data/FlightsDataAnalysis.scala
+++ spark-resilient-distributed-dataset/src/main/scala/org/spark/rdd/flights_data/FlightsDataAnalysis.scala
@@ -1,8 +1,11 @@
 package org.spark.rdd.flights_data
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object FlightsDataAnalysis {
+
+  case class Flight(date: String, delay: String, distance: String, origin: String, destination: String)
 
   def main(args: Array[String]): Unit = {
 
@@ -13,7 +16,14 @@ object FlightsDataAnalysis {
 
     val sc = spark.sparkContext
 
+    val readCsv = sc.textFile("datasource/flights/departuredelays.csv")
 
+    val flightsRdd: RDD[Flight] = readCsv.map(row => row.split(","))
+      .map(str => Flight(str(0), str(1), str(2), str(3), str(4))).mapPartitionsWithIndex {
+      (idx, iter) => if (idx == 0) iter.drop(1) else iter
+    }
+
+    println(flightsRdd.first())
 
   }
 
