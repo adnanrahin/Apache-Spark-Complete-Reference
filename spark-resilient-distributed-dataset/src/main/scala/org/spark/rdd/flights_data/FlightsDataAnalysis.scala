d1423d6eac0a9edd3c30cae9449ad9b8e22a6962
adnanrahin <adnan.rahin13@gmail.com>
2021-09-01 00:55:59 -0400

    All Delayed flights from ABQ Listed

--- spark-resilient-distributed-dataset/src/main/scala/org/spark/rdd/flights_data/FlightsDataAnalysis.scala
+++ spark-resilient-distributed-dataset/src/main/scala/org/spark/rdd/flights_data/FlightsDataAnalysis.scala
@@ -8,9 +8,13 @@ object FlightsDataAnalysis {
 
   case class Flight(date: String, delay: String, distance: String, origin: String, destination: String)
 
+  case class Airport(city: String, state: String, country: String, iata: String)
+
   def main(args: Array[String]): Unit = {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
+
+    /** Spark Configuration */
 
     val spark = SparkSession
       .builder().master("local[*]")
@@ -19,19 +23,33 @@ object FlightsDataAnalysis {
 
     val sc = spark.sparkContext
 
-    val readCsv = sc.textFile("datasource/flights/departuredelays.csv")
+    /** Flight CSV and RDD instantiated */
 
-    val flightsRdd: RDD[Flight] = readCsv.map(row => row.split(","))
+    val readFlightsCsv = sc.textFile("datasource/flights/departuredelays.csv")
+
+    val flightsRdd: RDD[Flight] = readFlightsCsv.map(row => row.split(","))
       .map(str => Flight(str(0), str(1), str(2), str(3), str(4))).mapPartitionsWithIndex {
       (idx, iter) => if (idx == 0) iter.drop(1) else iter
     }
+
+    // flightsRdd.foreach(row => println(row))
 
     /** @todo find all the flights from ABE -> ATL */
 
     val allFlightsFromAbeToAtl: RDD[Flight] = flightsRdd
       .filter(flight => flight.origin.equals("ABE") && flight.destination.equals("ATL"))
 
-    allFlightsFromAbeToAtl.foreach(row => println(row))
+    // allFlightsFromAbeToAtl.foreach(row => println(row))
+
+    /** @todo find all the flights that delayed origin ABQ */
+
+    val allDelayedABQFlights: RDD[(String, List[Flight])] = flightsRdd
+      .filter(flight => flight.origin.equals("ABQ") && flight.delay.toInt < 0)
+      .groupBy(_.origin).map(iter => (iter._1, iter._2.toList))
+
+    println(allDelayedABQFlights.getClass)
+
+    allDelayedABQFlights.foreach(row => println(row._1 + " " + row._2))
 
   }
 
