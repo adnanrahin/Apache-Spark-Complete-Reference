43f643183a779abe73264b028dce778976522019
adnanrahin <adnan.rahin13@gmail.com>
2021-09-02 01:32:18 -0400

    DOT flight data

---
+++ spark-flights-data-analysis/src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
@@ -0,0 +1,20 @@
+package org.flight.analysis
+
+object FlightDelaysAndCancellations {
+
+  case class Airport(iataCode: String,
+                     airport: String,
+                     city: String,
+                     state: String,
+                     country: String,
+                     latitude: String,
+                     longitude: String)
+
+  
+
+  def main(args: Array[String]): Unit = {
+
+
+  }
+
+}
