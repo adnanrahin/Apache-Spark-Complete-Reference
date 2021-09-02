3a111d75c8d5d2227222445eb1de1f92ac3b2859
adnanrahin <adnan.rahin13@gmail.com>
2021-09-02 01:46:34 -0400

    Case Class Created

--- spark-flights-data-analysis/src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
+++ spark-flights-data-analysis/src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
@@ -10,7 +10,16 @@ object FlightDelaysAndCancellations {
                      latitude: String,
                      longitude: String)
 
-  
+  case class Flight(year: String, month: String, day: String, dayOfWeek: String,
+                    airline: String, flightNumber: String, tailNumber: String, originAirport: String,
+                    destinationAirport: String, scheduledDeparture: String, departureTime: String,
+                    departureDelay: String, taxiOut: String, wheelsOut: String, scheduledTime: String,
+                    elapsedTime: String, airTime: String, distance: String, wheelsOn: String, taxiIn: String,
+                    scheduledArrival: String, arrivalTime: String, arrivalDelay: String, diverted: String,
+                    cancelled: String, cancellationsReason: String, airSystemDelay: String, securityDelay: String,
+                    airlineDelay: String, lateAircraftDelay: String, weatherDelay: String)
+
+  case class  Airline(iataCode: String, airlineName: String)
 
   def main(args: Array[String]): Unit = {
 
