01a34d22d9a10267e8d96bdf3d4ada9d6618b8e4
adnanrahin <adnan.rahin13@gmail.com>
2021-09-03 16:32:10 -0400

    Airport RDD

--- spark-flights-data-analysis/src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
+++ spark-flights-data-analysis/src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
@@ -47,9 +47,9 @@ object FlightDelaysAndCancellations {
 
   }
 
-  def loadFlightCsvToRDD(flightsCsv: RDD[String]): RDD[Flight] = {
+  def loadFlightCsvToRDD(flightsCSV: RDD[String]): RDD[Flight] = {
     val flightsRDD: RDD[Flight] =
-      flightsCsv
+      flightsCSV
         .map(row => row.split(",", -1))
         .map(str => Flight(str(0),
           str(1), str(2), str(3), str(4), str(5), str(6),
@@ -71,6 +71,18 @@ object FlightDelaysAndCancellations {
       }
 
     airlineRDD
+  }
+
+  def loadAirportToRDD(airportCSV: RDD[String]): RDD[Airport] = {
+    val airportRDD: RDD[Airport] =
+      airportCSV
+        .map(row => row.split(",", -1))
+        .map(str => Airport(str(0), str(1), str(2), str(3), str(4), str(5), str(6)))
+        .mapPartitionsWithIndex {
+          (idx, iter) => if (idx == 0) iter.drop(1) else iter
+        }
+
+    airportRDD
   }
 
 }
