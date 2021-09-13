64eea0549724b7615e7315c0eee924947eb0a4d0
adnanrahin <adnan.rahin13@gmail.com>
2021-09-13 00:40:01 -0400

    update

--- spark-flights-data-analysis/src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
+++ spark-flights-data-analysis/src/main/scala/org/flight/analysis/FlightDelaysAndCancellations.scala
@@ -136,7 +136,7 @@ object FlightDelaysAndCancellations {
       .map(airport => (airport.iataCode, airport.airport))
       .collect()
       .toMap
-    
+
     val notCancelledFlight =
       flightsRDD.filter(flight => flight.cancelled.equals("0"))
 
